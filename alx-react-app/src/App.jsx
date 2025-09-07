6f6c88ecdb9a089f2afa492e74036d7e46e3bee3
Deborah Mchau <deborahenry94@yahoo.com>
2025-09-07 14:13:13 +0300

    Modify JSX in a pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -24,6 +25,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <WelcomeMessage />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
