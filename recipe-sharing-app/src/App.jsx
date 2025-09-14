d12430c27f20774b0fd195c84d8ef841f92cac7b
Deborah Mchau <deborahenry94@yahoo.com>
2025-09-14 21:08:55 +0300

    Add first commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useRecipeStore from "./recipeStore";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial"}}>
+       <h1>Recipe Sharing App</h1>
+       <RecipeList />
+       <AddRecipeForm />
+    </div>  
+  )
+}
+
+export default App
