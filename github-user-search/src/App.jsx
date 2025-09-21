cbec15d62dc6d63a6bf9280696121ff288c26582
Deborah Mchau <deborahenry94@yahoo.com>
2025-09-21 16:18:21 +0300

    Set up API service with axios and env support

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+       <h1>GitHub User Search</h1>
+       <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.github.com",
+    headers: {
+        Authorization: 'token $ {import.meta.env.VITE_APP_GITHUB_API_KEY}',
+    },
+});
+
+export const searchUsers = async (username) => {
+    try {
+    const response = await axios.get('${BASE_URL}/users/${username}',{headers,});
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        throw error;
+    }
+};
+ 
