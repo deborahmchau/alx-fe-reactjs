380e6a23bf1fac5920a66263299d8b858edd846b
Deborah Mchau <deborahenry94@yahoo.com>
2025-10-18 17:03:52 +0300

    Implementing and Testing a Todo List Component in React

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TodoList from "./components/TodoList";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
